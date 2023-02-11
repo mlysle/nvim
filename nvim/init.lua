f07e80ff9095eece05ea7ef3ce38241b2b2b905c
Maxwell Lysle <max@lysle.org>
2023-02-10 23:41:56 -0700

    switched to lazy.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,21 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
 require('options')
 require('mappings')
+--require('packer-config')
+require('lazy-config')
 require('colors')
-require('packer-config')
 require('treesitter-config')
 require('mason-config')
 require('lsp-config.language-servers')
