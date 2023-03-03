e76964ee242f31e77392f0441c85e949ccacffe4
Maxwell Lysle <max@lysle.org>
2023-03-03 00:37:35 -0700

    TS now auto-installs useful languages

--- nvim/lua/treesitter-config/init.lua
+++ nvim/lua/treesitter-config/init.lua
@@ -3,7 +3,10 @@ local present, treesitter = pcall(require, "nvim-treesitter.configs")
 if not present then return end
 
 local options = {
-	--auto_install = true,
+	ensure_installed = { "lua", "racket", "bash", "html", "norg", "norg_meta", "python", "r", "markdown", "yaml", "latex",
+		"css",
+		"scss", "vim", "help" },
+	auto_install = true,
 
 	indent = {
 		enable = true
@@ -11,34 +14,34 @@ local options = {
 
 	highlight = {
 		enable = true,
-		additional_vim_regex_highlighting = {'org'},
+		additional_vim_regex_highlighting = { 'org' },
 	},
 
 	textobjects = {
 		select = {
-		  enable = true,
+			enable = true,
 
-		  -- Automatically jump forward to textobj, similar to targets.vim
-		  lookahead = true,
+			-- Automatically jump forward to textobj, similar to targets.vim
+			lookahead = true,
 
-		  keymaps = {
-			-- You can use the capture groups defined in textobjects.scm
-			["af"] = "@function.outer",
-			["if"] = "@function.inner",
-			["ac"] = "@conditional.outer",
-			["ic"] = "@conditional.inner",
-		  },
-		  -- You can choose the select mode (default is charwise 'v')
-		  selection_modes = {
-			['@parameter.outer'] = 'v', -- charwise
-			['@function.outer'] = 'V', -- linewise
-			['@class.outer'] = '<c-v>', -- blockwise
-		  },
-		  -- If you set this to `true` (default is `false`) then any textobject is
-		  -- extended to include preceding xor succeeding whitespace. Succeeding
-		  -- whitespace has priority in order to act similarly to eg the built-in
-		  -- `ap`.
-		  include_surrounding_whitespace = true,
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@conditional.outer",
+				["ic"] = "@conditional.inner",
+			},
+			-- You can choose the select mode (default is charwise 'v')
+			selection_modes = {
+				['@parameter.outer'] = 'v', -- charwise
+				['@function.outer'] = 'V', -- linewise
+				['@class.outer'] = '<c-v>', -- blockwise
+			},
+			-- If you set this to `true` (default is `false`) then any textobject is
+			-- extended to include preceding xor succeeding whitespace. Succeeding
+			-- whitespace has priority in order to act similarly to eg the built-in
+			-- `ap`.
+			include_surrounding_whitespace = true,
 		},
 	},
 }
