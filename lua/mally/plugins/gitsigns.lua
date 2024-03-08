b2f0ae352e5d397cbda059eaab255eae8f913e86
Maxwell Lysle <max@lysle.org>
2024-03-08 14:00:21 -0700

    added keybind to stage files

--- lua/mally/plugins/gitsigns.lua
+++ lua/mally/plugins/gitsigns.lua
@@ -1,5 +1,12 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = true,
+	"lewis6991/gitsigns.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require('gitsigns').setup()
+		local wk = require("which-key")
+		wk.register({
+			["<leader>h"] = { name = "+git" },
+			["<leader>hS"] = { "<cmd>Gitsigns stage_buffer<cr>", "Stage File" },
+		})
+	end,
 }
